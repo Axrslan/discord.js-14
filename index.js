a69e55f8055a0b70da17282946680361be2419b5
Axrslan <arsalangamerz0786@gmail.com>
2024-08-07 19:02:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const token = "";
+const PREFIX = "";RNTE
+
+bot.on("ready", () => {
+    console.log('i am ready!')
+
+})
+bot.login(token)
